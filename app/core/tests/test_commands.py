f03182f12fa32b5356a4ea9b868b112490621f8c
Parker Thomson <pdthomson1239@gmail.com>
2023-03-14 08:38:41 -0400

    Add functionality for our method to wait for the DB to be ready before connecting

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -3,14 +3,15 @@
 from unittest.mock import patch
 # mocking db behavior simulating db response
 
-from psycopg2 import OperationalError as Psycopg2Error
-# one of the possibilities we might get when connecting to the database before the DB is ready
+from psycopg2 import OperationalError as Psycopg2OpError
+# one possible error when connecting to the DB before its ready
 
 from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
-@patch('core.management.commands.wait_for_db.Command.check')
+
+@patch("core.management.commands.wait_for_db.Command.check")
 class CommandTests(SimpleTestCase):
     """Test Commands"""
 
@@ -19,16 +20,15 @@ class CommandTests(SimpleTestCase):
 
         call_command('wait_for_db')
 
-        patched_check.assert_called_once_with(database=['default'])
+        patched_check.assert_called_once_with(databases=['default'])
 
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """Test waiting for DB when getting OperationalError"""
-        patched_check.side_effect = [Psycopg2Error] * 2 + \
-            [OperationalError] * 3 + [True]
+        patched_check.side_effect = [Psycopg2OpError] * 2 + \
+                                    [OperationalError] * 3 + [True]
 
         call_command('wait_for_db')
 
         self.assertEqual(patched_check.call_count, 6)
-        patched_check.assert_called_once_with(database=['default'])
-
+        patched_check.assert_called_with(databases=['default'])
