69714fb6578c644f442530ed4096bd0a92015bfc
Parker Thomson <pdthomson1239@gmail.com>
2023-03-14 10:36:06 -0400

    Run flake8 and do linter spring cleaning on current code

--- app/core/models.py
+++ app/core/models.py
@@ -5,15 +5,19 @@ from django.contrib.auth.models import (
     PermissionsMixin
 )
 
+
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
-        # extra_fields are any number of Key Word Arguments(kwargs) something like name for instance
+        # extra_fields are any number of Key Word Arguments(kwargs) like name
         if not email:
             raise ValueError('User must have an email!')
-        user = self.model(email=self.normalize_email(email), **extra_fields)  # use to create a new user model using custom user
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        # use to create a new user model using custom user
         user.set_password(password)  # set encrypted password
-        user.save(using=self._db)  # self._db supports adding multiple databases to your project(this is best practice)
+        user.save(using=self._db)
+        # self._db supports adding multiple databases
+        # to your project(this is best practice)
 
         return user
 
@@ -26,9 +30,11 @@ class UserManager(BaseUserManager):
 
         return user
 
+
 class User(AbstractBaseUser, PermissionsMixin):
     # AbstractBaseUser has authentication functionality but NO field
-    # PermissionsMixin has functionality for django permissions feature and fields needed for that
+    # PermissionsMixin has functionality for
+    # django permissions feature and fields needed for that
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
