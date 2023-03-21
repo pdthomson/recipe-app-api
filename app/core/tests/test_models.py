762ceea702fc7e6fa5ba6e8a13d3ad571269df70
Parker Thomson <pdthomson1239@gmail.com>
2023-03-21 10:55:58 -0400

    create tag models tests, create the function to pass tests

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -4,6 +4,9 @@ from decimal import Decimal
 from core import models
 
 
+def create_user(email='user@exampl.com', password='testpass123'):
+    return get_user_model().objects.create_user(email, password)
+
 class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
@@ -56,3 +59,10 @@ class ModelTests(TestCase):
             description='Sample recipe description',
         )
         self.assertEqual(str(recipe), recipe.title)
+
+    def test_create_tag(self):
+        """Test creating a tag successfully"""
+        user = create_user()
+        tag = models.Tag.objects.create(user=user, name="Tag1")
+
+        self.assertEqual(str(tag), tag.name)
