f88617e411df2f5c734b17dc249cb81e827b05e8
Parker Thomson <pdthomson1239@gmail.com>
2023-03-22 09:42:09 -0400

    create a test for updating a tag for a user

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -12,6 +12,8 @@ from recipe.serializers import TagSerializer
 
 TAGS_URL = reverse('recipe:tag-list')
 
+def detail_url(tag_id):
+    return reverse('recipe:tag-detail', args=tag_id)
 
 def create_user(email='user@example.com', password='password123'):
     return get_user_model().objects.create_user(email=email, password=password)
@@ -59,3 +61,15 @@ class PrivateTagsApiTests(TestCase):
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], tag.name)
         self.assertEqual(res.data[0]['id'], tag.id)
+
+    def test_update_tag(self):
+        """test updating a tag"""
+        tag = Tag.objects.create(user=self.user, name='After Dinner')
+
+        payload = {'name': 'Dessert'}
+        url = detail_url(tag.id)
+        res = self.client.patch(url, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        tag.refresh_from_db()
+        self.assertEqual(tag.name, payload['name'])
